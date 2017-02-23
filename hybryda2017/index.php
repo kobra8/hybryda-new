fc10615329cfd15c065da63f3d87e87e1332f7b3
Admin <Admin>
2017-02-23 11:21:06 +0100

    Index sklejony

--- hybryda2017/index.php
+++ hybryda2017/index.php
@@ -1,46 +1,68 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title><?php wp_title() ?> </title>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<?php get_header(); ?>
 
-	<?php wp_head(); ?>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
 
-</head>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+         <h4>Nowe godziny otwarcia szkoły w okresie szkoleń komercyjnych</h4>
+          <p class="center-block">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div> 
+      <div class="item">
+          <h4>Wyniki rekrutacji na kurs j niemieckiego w Dębicy</h4>
+          <p class="center-block">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div> 
+       <div class="item">
+          <h4>Zapraszamy na kurs językowy</h4>
+          <p class="center-block">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       </div> 
+      
+    </div>
 
-<body>
-	
-	<header>
-	
-		<p>tu jest header <p>
-		
-	</header>
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div>
+  
+<div class="container text-center">    
+ <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <a href="#">
+        <img class="imgmenu"src="images/oferta.jpg" class="img-responsive" style="width:100%" alt="Image">
+        <p>Oferta szkoleniowa</p>
+      </a>
+    </div>
+    <div class="col-sm-4"> 
+      <a href="#">
+        <img class="imgmenu" src="images/sale.jpg" class="img-responsive" style="width:100%" alt="Image">
+        <p>Sale szkoleniowe</p> 
+      </a>
+    </div>
+      <div class="col-sm-4">
+        <a href="#">
+          <img class="imgmenu" src="images/projekty.jpg" class="img-responsive" style="width:100%" alt="Image">
+          <p>Realizowane projekty</p>
+        </a>
+    </div>
+  </div>
 
-	<div id="content">
+    
+</div><br>
 
-		<?php
-			if ( have_posts() ) :
-
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
-				
-					echo the_title($before, $after, $echo);
-					echo the_content();
-
-				endwhile;
-
-			endif; 
-		?>
-		
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<p> Tu jest stopka </p>
-	</footer>
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+  <?php get_footer(); ?>  
